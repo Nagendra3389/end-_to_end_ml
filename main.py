3ed1cb58444f3a0c04f90ddda12cbdc7b2295001
Nagendra babu <pnagendra341@gmail.com>
2024-01-20 13:57:26 +0530

    evalution and mlflow added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from mlproject import logger
 from mlproject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from mlproject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from mlproject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
+from mlproject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
+from mlproject.pipeline.stage05_model_evalution import ModelTrainerEvalutionPipeline
 
 STAGE_NAME = "Data ingestion stage"
 
@@ -34,6 +36,34 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Model Trainer stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+
+STAGE_NAME = "Model Evalution stage"
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerEvalutionPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
